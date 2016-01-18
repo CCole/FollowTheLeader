184d12330f00b5c00702bd16518726409e4820ec
Beastie <corypcole@gmail.com>
2016-01-17 22:41:56 -0500

    displayed raw JSON in body of html doc

--- Sheets.js
+++ Sheets.js
@@ -1,7 +1,7 @@
 var url = 'https://spreadsheets.google.com/feeds/list/1kpJkREh28K6-IGIOpFbDN2CrW8BmyPe-ZZghHB4w5H0/1/public/basic?alt=json' ;
 
 function callback(data){
-    $("#test").text = JSON.stringify(data);
+    $("#test").text(JSON.stringify(data));
 }
 
 
