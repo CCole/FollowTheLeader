5554209afce970bfbd4d4e39ab1998826c43f8cf
Beastie <corypcole@gmail.com>
2016-01-18 12:40:25 -0500

    removed some modules that I don't need and added a gitignore file

--- Sheets.js
+++ Sheets.js
@@ -1,8 +1,29 @@
 var url = 'https://spreadsheets.google.com/feeds/list/1kpJkREh28K6-IGIOpFbDN2CrW8BmyPe-ZZghHB4w5H0/1/public/basic?alt=json' ;
 
-function callback(data){
+/*function callback(data){
     $("#test").text(JSON.stringify(data));
+}*/
+
+function callback(data){
+    var rows = [];
+    var cells = data.feed.entry;
+    
+    for (var i = 0; i < cells.length; i++){
+        var rowObj = {};
+        rowObj.Participants = cells[i].title.$t;
+        var rowCols = cells[i].content.$t.split(',');
+        for (var j = 0; j < rowCols.length; j++){
+            var keyVal = rowCols[j].split(':');
+            rowObj[keyVal[0].trim()] = keyVal[1].trim();
+        }
+        rows.push(rowObj);
+    }
+    
+    var raw = document.createElement('p');
+    raw.innerText = JSON.stringify(rows);
+    document.body.appendChild(raw);
 }
+
 
 
 $(document).ready(function(){
